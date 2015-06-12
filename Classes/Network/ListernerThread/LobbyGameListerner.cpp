f06b2a4dd883a6877aa21e0bba51de172167eb32
shealyn <zhangheng189@163.com>
2015-06-12 10:00:17 +0800

    登录长连接大�

--- Classes/Network/ListernerThread/LobbyGameListerner.cpp
+++ Classes/Network/ListernerThread/LobbyGameListerner.cpp
@@ -29,6 +29,13 @@ void LobbyGameListerner::OnClose(TCPSocket* so, bool fromRemote)
 
 void LobbyGameListerner::OnError(TCPSocket* so, const char* e)
 {
+	ReadData rData;
+	rData.wMainCmdID = MDM_MB_SOCKET_FAILURE;
+	rData.wSubCmdID = SUB_GP_SOCKET_OPEN;
+	rData.wDataSize = 0;
+	memcpy(rData.sReadData, 0, 0);
+	//MessageQueue::pushQueue(rData);
+	MessageQueueLobby::pushQueue(rData);
 	CCLog("error--------------------<<%s>>",__FUNCTION__);
 }
 
@@ -48,17 +55,30 @@ bool LobbyGameListerner::OnMessage(TCPSocket* so,unsigned short	wSocketID, TCP_C
 	rData.wSubCmdID=Command.wSubCmdID;
 	rData.wDataSize=wDataSize;
 	memcpy(rData.sReadData,pDataBuffer,wDataSize);
-	//MessageQueue::pushQueue(rData);
 	MessageQueueLobby::pushQueue(rData);
 	return true;
 }
 void LobbyGameListerner::OnOpen(TCPSocket* so)
 {
+	CMD_GL_LogonAccounts LogonAccounts;
+	strcpy(LogonAccounts.szAccounts, DataModel::sharedDataModel()->sLogonAccount.c_str());
+	strcpy(LogonAccounts.szMachineID, "12");
+
+	MD5 m;
+	m.ComputMd5(DataModel::sharedDataModel()->sLogonPassword.c_str(), DataModel::sharedDataModel()->sLogonPassword.length());
+	std::string md5PassWord = m.GetMd5();
+	strcpy(LogonAccounts.szPassword, md5PassWord.c_str());
+
+	//连接大厅长连接
+	TCPSocket *tcp = TCPSocketControl::sharedTCPSocketControl()->getTCPSocket(SOCKET_LOBBY);
+	tcp->SendData(MDM_GL_C_DATA, SUB_GL_C_LOGON_ACCOUNTS, &LogonAccounts, sizeof(LogonAccounts));
+	
+	
+	/*//////////////////////////////////////////////////////////////////////////
 	ReadData rData;
-	rData.wMainCmdID=MDM_MB_SOCKET;
-	rData.wSubCmdID=SUB_GP_SOCKET_OPEN;
-	rData.wDataSize=0;
-	memcpy(rData.sReadData,0,0);
-	//MessageQueue::pushQueue(rData);
-	MessageQueueLobby::pushQueue(rData);
+	rData.wMainCmdID = MDM_MB_SOCKET;
+	rData.wSubCmdID = SUB_GP_SOCKET_OPEN;
+	rData.wDataSize = 0;
+	memcpy(rData.sReadData, 0, 0);
+	MessageQueueLobby::pushQueue(rData);*/
 }
