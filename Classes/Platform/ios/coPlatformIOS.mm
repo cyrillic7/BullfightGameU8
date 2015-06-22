2a33fbd0c8e59a23922629b54c3c3a8133cf34f0
张恒 <zhangheng189@163.com>
2015-06-22 20:16:22 +0800

    iOS获取uuid

--- Classes/Platform/ios/coPlatformIOS.mm
+++ Classes/Platform/ios/coPlatformIOS.mm
@@ -4,7 +4,6 @@
 #include "../coPlatform.h"
 #include "../../Libs/JsonAide.h"
 USING_NS_CC;
-
 //---------------------------------------------------------------------------
 /**
  * 执行动作
@@ -23,7 +22,32 @@ std::string platformAction(const std::string& jsonString)
 	}
 
 	int act = aide.getInt("act", -1);
-
+    switch (act) {
+        case 100:
+        {
+           // NSString *retrieveuuid = [SSKeychain passwordForService:@"com.mohe.userinfo"account:@"uuid"];
+            
+            //if ( retrieveuuid == nil || [retrieveuuid isEqualToString:@""]){
+                CFUUIDRef uuid = CFUUIDCreate(NULL);
+                assert(uuid != NULL);
+                CFStringRef uuidStr = CFUUIDCreateString(NULL, uuid);
+                
+                NSString *retrieveuuid = [NSString stringWithFormat:@"%@", uuidStr];
+                
+                //[SSKeychain setPassword:retrieveuuid forService:@"com.mohe.userinfo"account:@"uuid"];D9661007-6BC0-4F49-AA79-1C996E601AFE
+            //27C54597-2384-4995-8C0C-D1688F81A965
+           // }
+            // const charchar *cStr = [[[NSProcessInfo processInfo] globallyUniqueString] UTF8String];
+            NSLog(@"---:%@",retrieveuuid);
+           // const char *cStr =[[[NSProcessInfo processInfo] globallyUniqueString] UTF8String];
+            //return [str str];
+            return [retrieveuuid UTF8String];
+        }
+            break;
+            
+        default:
+            break;
+    }
 	CCLog("ios.............................  %d",act);
 
 	return "";
