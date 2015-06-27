9cfa70ce7c8d3c8470c264ac41515f86e1f9cf52
张恒 <zhangheng189@163.com>
2015-06-27 18:22:45 +0800

    添加音效配置文件

---
+++ Classes/Tools/SoundConfig.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#define kMusicHundred "music/"                                 //百人牛牛背音乐
+/*
+#define kMusicGameMenu "music/m_menu.mp3"
+#define kMusicGameIn "music/m_gameIn.mp3"
+
+//主菜单////////////////////////////////////////////////////////////////////////
+#define kSoundPlane "sound/s_plane.mp3"//飞机
+#define kSoundPlaneBomb "sound/s_plane_bomb.mp3"
+
+//剧情////////////////////////////////////////////////////////////////////////
+
+#define kSoundKillPeople "sound/s_kill_people.wav"
+
+#define kSoundBomb(name) name==0?"sound/s_gun_bomb0.mp3" : "sound/s_gun_bomb1.wav"
+#define kSoundGun(name) CCString::createWithFormat("sound/s_gun%d.mp3",(name))->getCString()
+#define kSoundEnemyDeath(name) CCString::createWithFormat("sound/s_enemy_death%d.mp3",(name))->getCString()
+#define kSoundBaseUnderAttack(name) CCString::createWithFormat("sound/s_base_under_attack%d.wav",(name))->getCString()
+
+#define kSoundBuySucceed "sound/s_buy_succeed.mp3"
+#define kSoundSwitchGun "sound/s_switch_gun.mp3"
+
+#define kSoundShootBomb "sound/s_shoot_bomb.mp3"
+#define kSoundSelect "sound/s_select.mp3"
+#define kSoundBack "sound/s_back.mp3"
+#define kSoundSkill1 "sound/s_skill_1.mp3"*/
