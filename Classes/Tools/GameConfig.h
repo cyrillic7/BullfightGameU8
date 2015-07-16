3c8707880a7705a049135928eb17ab11a0ef778d
张恒 <zhangheng189@163.com>
2015-07-16 14:37:37 +0800

    1.0.5

--- Classes/Tools/GameConfig.h
+++ Classes/Tools/GameConfig.h
@@ -2,7 +2,7 @@
 
 
 #define DEBUG_TEST 1 //0调试模式、1测试模式、2发布模式
-#define GAME_VERSION "Version1.0.4"//游戏版本号
+#define GAME_VERSION "Version1.0.5"//游戏版本号
 
 #define VERSION_FRAME				16777217
 #define VERSION_CLIENT				17170433
